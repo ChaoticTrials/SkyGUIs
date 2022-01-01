5a5b7bf7cda4814bf2f9b88ebc6fb9db3a0d23f0
MelanX <info@melanx.de>
2022-01-01 19:48:50 +0100

    add keybind

--- src/main/java/de/melanx/easyskyblockmanagement/EasySkyblockManagement.java
+++ src/main/java/de/melanx/easyskyblockmanagement/EasySkyblockManagement.java
@@ -8,10 +8,14 @@ import io.github.noeppi_noeppi.libx.mod.registration.ModXRegistration;
 import io.github.noeppi_noeppi.libx.mod.registration.RegistrationBuilder;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fmlclient.registry.ClientRegistry;
 
 import javax.annotation.Nonnull;
 
@@ -32,6 +36,11 @@ public final class EasySkyblockManagement extends ModXRegistration {
 
         instance = this;
         network = new EasyNetwork(this);
+
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+            ClientRegistry.registerKeyBinding(Keybinds.ALL_TEAMS);
+            MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
+        });
     }
 
     public static EasySkyblockManagement getInstance() {
