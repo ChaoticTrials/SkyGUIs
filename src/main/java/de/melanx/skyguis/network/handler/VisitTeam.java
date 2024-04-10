655a223ef6e15f3636c9b5d2c966a5b0091e781c
MelanX <info@melanx.de>
2024-04-10 19:23:35 +0200

    Add newly added visit command cooldown

--- src/main/java/de/melanx/skyguis/network/handler/VisitTeam.java
+++ src/main/java/de/melanx/skyguis/network/handler/VisitTeam.java
@@ -3,6 +3,7 @@ package de.melanx.skyguis.network.handler;
 import de.melanx.skyblockbuilder.config.common.PermissionsConfig;
 import de.melanx.skyblockbuilder.data.SkyblockSavedData;
 import de.melanx.skyblockbuilder.data.Team;
+import de.melanx.skyblockbuilder.util.RandomUtility;
 import de.melanx.skyblockbuilder.util.WorldUtil;
 import de.melanx.skyguis.SkyGUIs;
 import de.melanx.skyguis.network.EasyNetwork;
@@ -48,6 +49,12 @@ public record VisitTeam(UUID team) {
                 return true;
             }
 
+            if (!player.hasPermissions(2) && !data.getOrCreateMetaInfo(player).canVisit(level.getGameTime())) {
+                network.handleLoadingResult(ctx.get(), LoadingResult.Status.FAIL, Component.translatable("skyblockbuilder.command.error.cooldown",
+                        RandomUtility.formattedCooldown(PermissionsConfig.Teleports.visitCooldown - (level.getGameTime() - data.getOrCreateMetaInfo(player).getLastVisitTeleport()))));
+                return true;
+            }
+
             if (!team.allowsVisits() && !player.hasPermissions(1)) {
                 network.handleLoadingResult(ctx.get(), LoadingResult.Status.FAIL, Component.translatable("skyblockbuilder.command.disabled.visit_team"));
                 return true;
@@ -58,6 +65,7 @@ public record VisitTeam(UUID team) {
                 return true;
             }
 
+            data.getOrCreateMetaInfo(player).setLastVisitTeleport(level.getGameTime());
             WorldUtil.teleportToIsland(player, team);
             network.handleLoadingResult(ctx.get(), LoadingResult.Status.SUCCESS, Component.translatable("skyblockbuilder.command.success.visit_team", team.getName()));
             return true;
